d37c436c746064b56e9bb1e4965ab74950a9d032
refatkhan <refatkhan897@gmail.com>
2025-02-09 19:50:02 +0600

    while loop task 3 complete

--- while/while (3).js
+++ while/while (3).js
@@ -16,13 +16,25 @@ Display sum of all the even numbers from 206 to 311.
  */
 
 /*programming hero*/
-let i =81;
+let i = 81;
 let sum = 0;
 while(i <=131){
     if(i %2 ===1){
-        console.log("Next number is: ",i,"\nnow add this number :",i);
         sum = sum+i;
-        console.log("Total sum is: ", sum);
+        console.log("Total sum is with previous: ", sum);
     }
     i++;
+}
+// console.log("Total sum is: ", sum);
+
+// all even number 
+console.log("start even sum");
+let j = 206;
+let evensum = 0;
+while(j <=311){
+    if(j %2 === 0){
+        evensum = evensum+i;
+        console.log("Total sum is with previous: ", evensum);
+    }
+    j++;
 }

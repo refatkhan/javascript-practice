ea42d018dc178e8f20f12270fd81b705ccd2b071
refatkhan <refatkhan897@gmail.com>
2025-02-09 17:37:09 +0600

    basic while loop task-2

---
+++ while/whileloop.js
@@ -0,0 +1,36 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+/*programming hero*/
+
+// Finding all odd numbers
+let i =61;
+while(i <100){
+    if(i%2===1){
+        console.log(i);
+    }
+    i++;
+}
+// find even number 
+let j = 78;
+console.log("Start Even number");
+while(j<98){
+    if(j%2 === 0){
+        console.log(j);
+    }
+    j++;
+}

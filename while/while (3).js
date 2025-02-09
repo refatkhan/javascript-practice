902a38cffa27d79cb8200f7378fc51ebb653debb
refatkhan <refatkhan897@gmail.com>
2025-02-09 17:43:59 +0600

    basic third problem solved

--- while/while (3).js
+++ while/while (3).js
@@ -16,3 +16,13 @@ Display sum of all the even numbers from 206 to 311.
  */
 
 /*programming hero*/
+let i =81;
+let sum = 0;
+while(i <=131){
+    if(i %2 ===1){
+        console.log("Next number is: ",i,"\nnow add this number :",i);
+        sum = sum+i;
+        console.log("Total sum is: ", sum);
+    }
+    i++;
+}

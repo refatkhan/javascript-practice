ac036584e0eb621887d078f0759f91ddf7ed2ee7
refatkhan <refatkhan897@gmail.com>
2025-02-09 17:07:04 +0600

    while loop chellange

---
+++ while/while (1).js
@@ -0,0 +1,12 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+/*programming hero*/
+let i = 1;
+while(i<=60){
+    console.log("i will invest at least 6 hours every single day", i);
+    i++;
+}

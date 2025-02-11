757ab03bd5b949991c9e085a5fc804f2ba3c7344
refatkhan <refatkhan897@gmail.com>
2025-02-11 11:10:42 +0600

    reverse a array technique

--- looping.js
+++ looping.js
@@ -9,5 +9,9 @@
 
 const friends = ["elon",'bill','mark']
 for(const friend of friends){
-    console.log(friend);
+    // console.log(friend);
+}
+for (let i = 0; i<friends.length; i++){
+    // console.log(i);
+    console.log("Number of index: "+ i + " value is -----> " + friends[i]);
 }

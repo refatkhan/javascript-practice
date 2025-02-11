9d4cf7a8b2de478265f2f6d2286e699c551253ad
refatkhan <refatkhan897@gmail.com>
2025-02-11 11:19:57 +0600

    reverse using array and unshift function

--- reverse.js
+++ reverse.js
@@ -1,4 +1,13 @@
 const numbers = [1,2,3,4,5,6]
 // reverse using reverse function
-numbers.reverse();
-console.log(numbers);
+// numbers.reverse();
+// console.log(numbers);
+
+// reverse using unshift ar array
+
+const rev_num = []
+for (const rev_nums of numbers){
+    rev_num.unshift(rev_nums);
+    
+}
+console.log(rev_num);

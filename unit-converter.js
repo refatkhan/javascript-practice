931ad59523b403e9327568bdc867a757dd0d7bcf
refatkhan <refatkhan897@gmail.com>
2025-02-14 01:15:15 +0600

    add more feature

--- unit-converter.js
+++ unit-converter.js
@@ -1,7 +1,9 @@
 function inchToFeet(inch){
-    const feet = inch/12;
-    return feet;
+    const feet = parseInt(inch / 12);
+    const inches = inch % 12;
+    const result = feet + " feet " + inches + " inches"
+    return result;
 }
 const height = inchToFeet(75);
-// console.log(height);
+console.log(height);
 // console.log(inchToFeet(80).toFixed(2));

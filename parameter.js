ef8a83f8868d50dbc5544736d777f7fe24690293
refatkhan <refatkhan897@gmail.com>
2025-02-13 17:30:31 +0600

    string odd even check with function

---
+++ parameter.js
@@ -0,0 +1,9 @@
+function evenStrCheck(str){
+
+    if(str.length %2 === 0){
+        console.log(`The string ${str} is even`);
+    }else{
+        console.log(`The string ${str} is odd`);
+    }
+}
+evenStrCheck('Dhaka');

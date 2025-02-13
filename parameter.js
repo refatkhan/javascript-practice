377b344c7b0746965f452bc8e6fc8b2944187f9d
refatkhan <refatkhan897@gmail.com>
2025-02-13 18:25:47 +0600

    take only even number and make a array from a array

--- parameter.js
+++ parameter.js
@@ -1,9 +1,25 @@
-function evenStrCheck(str){
-
-    if(str.length %2 === 0){
-        console.log(`The string ${str} is even`);
-    }else{
-        console.log(`The string ${str} is odd`);
-    }
+function evenStrCheck(str) {
+  if (str.length % 2 === 0) {
+    console.log(`The string ${str} is even`);
+  } else {
+    console.log(`The string ${str} is odd`);
+  }
 }
-evenStrCheck('Dhaka');
+// evenStrCheck("Dhaka"); 
+
+// create a function that will return only even numbers
+
+function evenNumbersOnly(numbers){
+    const evenArray = [];
+for (const number  of numbers) {
+       if(number %2 === 0){
+        console.log(number);
+        evenArray.push(number);
+       }
+       
+    }
+    return evenArray;
+}
+const numbers =[5,6,9,8,32,5];
+const evens = evenNumbersOnly(numbers);
+console.log(evens);

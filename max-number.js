d0b2e3b79832a143016b30003dc56900e3500fc8
refatkhan <refatkhan897@gmail.com>
2025-02-15 00:21:17 +0600

    find max number from an array

--- max-number.js
+++ max-number.js
@@ -9,9 +9,23 @@
 //     }
 // }
 // const big = getMax(45,69,36);
-function biggerNumber(max){
-    console.log("Max number is",max);
-    return 0;
-}
-const num = biggerNumber(Math.max(12,69,3,6,58,96,36,78))
+// function biggerNumber(max){
+//     console.log("Max number is",max);
+//     return 0;
+// }
+// const num = biggerNumber(Math.max(12,69,3,6,58,96,36,78))
 // console.log(num);
+
+const heights = [65,69,36,98,75,12,5,8];
+function getMax(numbers){
+    let max = numbers[0];
+    for (const num of numbers){
+        if(num > max){
+            max = num;
+        }
+    }
+    return max;
+}
+
+const max = getMax(heights);
+console.log("highest height is: ",max);

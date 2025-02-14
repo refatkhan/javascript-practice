2bf62a18d111d7df002ffb8b06ab5d61d88847bd
refatkhan <refatkhan897@gmail.com>
2025-02-15 00:09:05 +0600

    find gatter number using math.max

---
+++ max-number.js
@@ -0,0 +1,17 @@
+
+// function getMax(jim , mim , dim){
+//     if(jim > mim && jim > dim){
+//         console.log("jim is bigger");
+//     }else if(mim > jim && mim > dim){
+//         console.log("mim is bigger");
+//     }else{
+//         console.log("dim is bigger");
+//     }
+// }
+// const big = getMax(45,69,36);
+function biggerNumber(max){
+    console.log("Max number is",max);
+    return 0;
+}
+const num = biggerNumber(Math.max(12,69,3,6,58,96,36,78))
+// console.log(num);
